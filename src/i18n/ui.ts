9ddbf1fdcda3c276cae124ace6b9da1c38983425
Julio <jazurduymontalvo@gmail.com>
2025-06-04 15:19:16 +0200

    Booking

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,11 +8,13 @@ export const defaultLenguage = "es";
 export const ui = {
     es: {
         'nav.home': 'Inicio',
+        'nav.booking': 'Reserva',
         'nav.about': 'Sobre nosotros',
         'nav.contact': 'Contacto',
       },
     eus: {
         'nav.home': 'Hasiera',
+        'nav.booking': 'Erreserba',
         'nav.about': 'Nor gara',
         'nav.contact': 'Kontaktua',
       },
