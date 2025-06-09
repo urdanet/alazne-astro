b9ac6b6f871c4f0294b2a1a305db270e8c237a03
Julio <jazurduymontalvo@gmail.com>
2025-06-09 16:45:25 +0200

    Add: Adapter node to run build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   // ...
   integrations: [
     tailwind(), // aquí es donde va la integración de Astro + Tailwind
   ],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
