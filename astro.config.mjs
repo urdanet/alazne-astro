77f2e5cc7ee7750a08b96244c6393b877ce1c676
Julio <jazurduymontalvo@gmail.com>
2025-06-17 14:24:29 +0200

    Add unplugi incons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 import preact from '@astrojs/preact';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
   // ...
@@ -10,7 +11,15 @@ export default defineConfig({
     preact(),
     tailwind(), // aquí es donde va la integración de Astro + Tailwind
   ],
-
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'jsx',        // genera componentes para JSX/TSX
+        jsx: 'preact',          // usa Preact (o 'react' si fuera React)
+        autoInstall: true,      // instala packs faltantes al vuelo
+      }),
+    ],
+  },
   adapter: node({
     mode: 'standalone',
   }),
